7c32dfd70b4100b4137c33fef6ddcecb840707d0
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-14 21:19:27 +0200

    Create show and new artist pages

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,28 @@
 class ArtistsController < ApplicationController
+
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist_params = params.require(:artist).permit(:name)
+
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to @artist
+    else
+      render "new"
+    end
+  end
+  
+
 end
