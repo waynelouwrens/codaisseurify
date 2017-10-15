db38ec1542c3fdf723431400ffdf1da7c0676d93
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-15 20:22:20 +0200

    Create photo views

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,8 @@
+class PhotosController < ApplicationController
+  def destroy
+    photo = Photo.find(params[:id])
+      @artist = photo.artist
+        photo.destroy
+          redirect_to edit_artist_path(@artist), notice: "Photo successfully removed"
+  end
+end
