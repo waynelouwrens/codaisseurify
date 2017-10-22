c40de19c37c7c8b70f7e8921c3ee3a7bee86873b
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-22 11:11:08 +0200

    Add API artist show & create

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,9 +1,38 @@
 class Api::ArtistsController < ApplicationController
 
+   skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: {
       artists: Artist.all
     }.to_json
   end
 
+  def show
+    artist = Artist.find(params[:id])
+
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+
+  def create
+    artist = Artist.new(artist_params)
+
+    if artist.save
+      render status: 201, json: {
+        message: "Artist successfully created",
+        artist: artist
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: artist.errors
+      }.to_json
+    end
+  end
+
+    private
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
 end
